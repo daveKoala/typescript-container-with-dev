29a89a46be29af67653cedb75a2d930863cec4fd
david clare <davidclare@davids-MacBook-Pro.local>
2023-05-31 08:48:33 +0100

    Gettining started

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
 up:
-	docker-compose -f ./app/docker-compose.yml up --build
+	docker-compose --project-name core -f ./app/docker-compose.yml up --build 
+
+up-prod:
+	docker-compose --project-name core -f ./app/docker-compose.yml -f ./app/docker-compose.prod.yml up --build -d
+
+down:
+	docker-compose down
+
+dev:
+	npm run dev --prefix ./app
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import 'dotenv/config';
 
 const app = express();
 
 app.use(morgan(':method :url :status - :response-time ms'))
 
 app.use("/pingz", (req: Request, res: Response) => {
-  res.status(200).send("OK")
+  const details = {
+    buildId: process.env.BUILD_ID || "n/a"
+  }
+  res.status(200).json(details)
 });
 
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
+  console.log(`Build ID: ${process.env.BUILD_ID}`)
 });
