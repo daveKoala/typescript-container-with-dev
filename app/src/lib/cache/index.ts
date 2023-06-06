985473a31925934dd9f123dbac491e0171062300
david clare <davidclare@davids-MacBook-Pro.local>
2023-06-06 09:30:24 +0100

    Ready to cache

--- app/src/lib/cache/index.ts
+++ app/src/lib/cache/index.ts
@@ -2,6 +2,7 @@ import * as redis from 'redis';
 
 interface Cache {
     redisClient: redis.RedisClientType | null;
+    error: Error | null;
     connect: (cacheURL: string, cachePassword: string) => Promise<Cache>;
     ping: () => Promise<string>;
     write: () => Promise<Cache>;
@@ -10,29 +11,37 @@ interface Cache {
 
 export const cache: Cache = {
     redisClient: null,
+    error: null,
 
-   async  connect(cacheURL: string, cachePassword: string) {
-        this.redisClient = redis.createClient({
-            // rediss for TLS
-            url: cacheURL,
-            password: cachePassword
-        })
+    async connect(cacheURL: string, cachePassword: string) {
+        try {
+            this.redisClient = redis.createClient({
+                // rediss for TLS
+                url: cacheURL,
+                password: cachePassword
+            })
 
-        this.redisClient.on('error', (error) => console.error(error)).on('connect', () => console.log("Cache connected"));
-        await this.redisClient?.connect()
-        return this;
+            this.redisClient.on('error', (error) => console.error(error)).on('connect', () => console.log("Cache connected"));
+            await this.redisClient?.connect()
+        } catch (error) {
+            if(error instanceof Error) {
+                this.error = error;
+            }
+        } finally {
+            return this;
+        }
     },
 
     async ping() {
         const pingCommandResult = await this.redisClient?.ping();
         console.log('Ping command result: ', pingCommandResult);
 
-        if(pingCommandResult != 'PONG') return 'Cache not connected'
-        
-        return `${pingCommandResult}: cache connected`;
-      },
+        if (pingCommandResult != 'PONG') return 'Cache not connected'
 
-    async write() { 
+        return `${pingCommandResult}: cache connected`;
+    },
+
+    async write() {
         return this;
     },
 
